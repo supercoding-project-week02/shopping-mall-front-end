4fafd3a3944e04a90e2697ea57380370a9dbc393
kimseunggyu <dev.seunggyu@gmail.com>
2023-08-15 06:31:09 +0900

    chore : vite react js 기반 path alias 설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import jsconfigPaths from 'vite-jsconfig-paths';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), jsconfigPaths()],
+});
