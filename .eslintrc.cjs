8c4763d9aba756813c9d2ef235ea0431a7eed408
kimseunggyu <dev.seunggyu@gmail.com>
2023-08-15 21:50:17 +0900

    feat : Icon 컴포넌트 구현 및 사용 방법 정의 - Sample 에 적용함

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -23,5 +23,6 @@ module.exports = {
         ],
       },
     ],
+    "react/prop-types": "off"
   },
 };
---
+++ src/components/common/Icon/Icon.jsx
@@ -0,0 +1,45 @@
+/** @jsxImportSource @emotion/react */
+import PropTypes from 'prop-types';
+import { css } from '@emotion/react';
+
+import * as icon from '@/assets';
+
+const DEFAULT_SIZE = 24;
+
+export const Icon = ({
+  name,
+  size = DEFAULT_SIZE,
+  width,
+  height,
+  color = '#595959',
+  fill,
+  style,
+  ...rest
+}) => {
+  const SVGIcon = icon[name];
+  const IconStyles = css`
+    &,
+    path {
+      ${color ? `stroke: ${color} !important;` : ''}
+      ${fill ? `fill: ${color} !important;` : ''}
+      width: ${width ?? `${size}px`};
+      height: ${height ?? `${size}px`};
+    }
+    circle {
+      ${color ? `stroke: ${color} !important;` : ''}
+      ${fill ? `fill: ${color} !important;` : ''}
+    }
+  `;
+
+  return <SVGIcon {...rest} css={IconStyles} style={style} />;
+};
+
+// 타입스크립트 안되어서 자동 완성을 위한 해당 기능 추가
+Icon.propTypes = {
+  name: PropTypes.arrayOf(icon.ICON_NAME).isRequired,
+  size: PropTypes.number,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  color: PropTypes.string,
+  fill: PropTypes.string,
+};
