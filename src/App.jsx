e5cfb1409bfdfc7a6f3b3165cf534cc245d1f4e7
YukDDong <ekfhd5537@naver.com>
2023-08-15 15:10:19 +0900

    feat: router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import { Fragment } from 'react';
 
-import { Sample } from '@/components/Sample';
 import './App.css';
-import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Main from './pages/Main';
 import Detail from './pages/Detail';
+import MainHeader from './layouts/MainHeader';
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <Main/>
+      path: '/',
+      element: <Main />,
     },
     {
       // id값으로 처리할 때 아래 주석을 풀고 진행 하시면 됩니다.
-      // path: "/detail/:id" 
-      path: "/detail",
-      element: <Detail/>
+      // path: "/detail/:id"
+      path: '/detail',
+      element: <Detail />,
     },
-  ])
+  ]);
   return (
     <Fragment>
-      <Sample/>
+      <MainHeader />
+      <RouterProvider router={router} />
     </Fragment>
   );
 }
