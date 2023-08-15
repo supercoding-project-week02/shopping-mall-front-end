156a8bb5cf16938a358b1875d5faaf196381fb7e
kimseunggyu <dev.seunggyu@gmail.com>
2023-08-15 21:49:06 +0900

    feat : Icon 이미지 추가

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+export { ReactComponent as IconBasket } from './iconBasket.svg';
+export { ReactComponent as IconSearch } from './iconSearch.svg';
+export { ReactComponent as IconUser } from './iconUser.svg';
+
+// 타입스크립트를 적용하지 않아서 해당 부분 추가
+// IconName 을 추가해야함!
+export const ICON_NAME = [
+  'IconBasket', //
+  'IconSearch',
+  'IconUser',
+];
