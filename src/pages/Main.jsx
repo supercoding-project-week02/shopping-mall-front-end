8435694fa70b45d5fb408733cade4fdbf8b73ec8
Joojue <128790356+Joojue@users.noreply.github.com>
2023-08-16 13:30:36 +0900

    캐러셀 작업 완료

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,12 @@
+import CarouselBox from '@/components/main/carousel/CarouselBox';
 import * as S from '@/components/main/Styles.js';
 
 const Main = () => {
   return (
     <S.MainPageWrapper>
-      <S.MainContainer />
+      <S.MainContainer>
+        <CarouselBox />
+      </S.MainContainer>
     </S.MainPageWrapper>
   );
 };
