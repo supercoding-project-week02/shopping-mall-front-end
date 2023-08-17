1bde4d1376a3cc79d09a876933e8ad0a0c9ef71c
kimseunggyu <dev.seunggyu@gmail.com>
2023-08-17 11:35:51 +0900

    feat : User Info 를 임시로 전역상태(context) 로 변경 및 적용

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,67 +1,72 @@
-import { useState } from 'react';
+import { useModal } from '@ebay/nice-modal-react';
 
 import TextField from '@/components/common/TextField/TextField.jsx';
+import PayMoneyModal from '@/components/modals/PayMoneyMoal.jsx';
+import ShippingAddressModal from '@/components/modals/ShippingAddressModal.jsx';
+import { useUserInfo } from '@/contexts/UserInfo.jsx';
 import * as S from './UserProfile.styles.jsx';
 
-const USER = {
-  email: 'helloworld@gmail.com',
-  password: '1234',
-  name: 'Tester',
-  phoneNumber: '010-1234-5678',
-  payMoney: 20000,
-  address: '서울시 마포구',
-};
-
 const UserProfile = () => {
-  const [user, setUser] = useState(USER);
+  const { user, onChangeUser } = useUserInfo();
+  const payMoneyModal = useModal(PayMoneyModal);
+  const shippingAddressModal = useModal(ShippingAddressModal);
 
-  const ChargeButton = <S.Button>충전</S.Button>;
-  const AddressButton = <S.Button>배송지 변경</S.Button>;
-
-  const handleChangeUser = (name, value) => {
-    setUser({
-      ...user,
-      [name]: value,
-    });
-  };
+  const ChargeButton = (
+    <S.Button
+      onClick={() => {
+        payMoneyModal.show();
+      }}
+    >
+      충전
+    </S.Button>
+  );
+  const AddressButton = (
+    <S.Button
+      onClick={() => {
+        shippingAddressModal.show();
+      }}
+    >
+      배송지 변경
+    </S.Button>
+  );
 
   return (
-    <S.UserProfileWrapper>
-      <S.Avatar src="" alt="avatar" />
-      <S.UserInfoWrapper>
-        <TextField inputType="email" label="이메일" value={user.email} />
-        <S.Line />
-        <TextField
-          inputType="password"
-          name="password"
-          label="비밀번호"
-          value={user.password}
-          editable
-          onSubmit={handleChangeUser}
-        />
-        <S.Line />
-        <TextField
-          label="이름"
-          name="name"
-          value={user.name}
-          editable
-          onSubmit={handleChangeUser}
-        />
-        <S.Line />
-        <TextField
-          label="전화번호"
-          name="phoneNumber"
-          value={user.phoneNumber}
-          editable
-          onSubmit={handleChangeUser}
-        />
-        <S.Line />
-        <TextField label="페이머니" value={user.payMoney} rightSlot={ChargeButton} />
-        <S.Line />
-        <TextField label="배송지" value={user.address} rightSlot={AddressButton} />
-        <S.Line />
-      </S.UserInfoWrapper>
-    </S.UserProfileWrapper>
+    <>
+      <S.UserProfileWrapper>
+        <S.Avatar src="" alt="avatar" />
+        <S.UserInfoWrapper>
+          <TextField inputType="email" label="이메일" value={user.email} />
+          <S.Line />
+          <TextField
+            inputType="password"
+            name="password"
+            label="비밀번호"
+            value={user.password}
+            editable
+            onSubmit={onChangeUser}
+          />
+          <S.Line />
+          <TextField label="이름" name="name" value={user.name} editable onSubmit={onChangeUser} />
+          <S.Line />
+          <TextField
+            label="전화번호"
+            name="phoneNumber"
+            value={user.phoneNumber}
+            editable
+            onSubmit={onChangeUser}
+          />
+          <S.Line />
+          <TextField
+            label="페이머니"
+            value={user.payMoney.toLocaleString('ko-KR')}
+            rightSlot={ChargeButton}
+          />
+          <S.Line />
+          <TextField label="배송지" value={user.address} rightSlot={AddressButton} />
+          <S.Line />
+        </S.UserInfoWrapper>
+      </S.UserProfileWrapper>
+    </>
   );
 };
 
---
+++ src/contexts/UserInfo.jsx
@@ -0,0 +1,47 @@
+// TODO: API 나오면 해당 부분 제거 할 수도 있음
+import { createContext, useContext, useState } from 'react';
+
+export const UserInfoContext = createContext(null);
+
+const USER = {
+  email: 'helloworld@gmail.com',
+  password: '1234',
+  name: 'Tester',
+  phoneNumber: '010-1234-5678',
+  payMoney: 20000,
+  address: '서울시 마포구',
+};
+export const UserInfoProvider = ({ children }) => {
+  const [user, setUser] = useState(USER);
+
+  const handleChangeUser = (name, value) => {
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleChargePayMoney = (amount) => {
+    handleChangeUser('payMoney', user.payMoney + Number(amount));
+  };
+
+  return (
+    <UserInfoContext.Provider
+      value={{
+        user,
+        onChangeUser: handleChangeUser,
+        onChargePayMoney: handleChargePayMoney,
+      }}
+    >
+      {children}
+    </UserInfoContext.Provider>
+  );
+};
+
+export const useUserInfo = () => {
+  const userInfoContext = useContext(UserInfoContext);
+  if (!userInfoContext) {
+    throw new Error('Could Not Found UserInfoContext');
+  }
+  return userInfoContext;
+};
