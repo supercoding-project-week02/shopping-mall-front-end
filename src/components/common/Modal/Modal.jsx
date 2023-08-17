8f29f75487f7cb153c8b8a35409202110d892013
kimseunggyu <dev.seunggyu@gmail.com>
2023-08-17 17:27:55 +0900

    refactor : Icon, Modal 컴포넌트 설명 및 사용법 작성

--- src/components/common/Modal/Modal.jsx
+++ src/components/common/Modal/Modal.jsx
@@ -4,6 +4,14 @@ import Portal from '@/components/common/Portal.jsx';
 import { useOutsideClick } from '@/hooks/useOutsideClick.js';
 import * as S from './Modal.styles.jsx';
 
+/**
+ * @desc 기본 Modal UI 를 담당합니다. 모달 영역 밖은 dimmed UI 를 표시하고 modal contents 영역을 표현합니다.
+ * @example
+ * <Modal visible={visible} onClose={onClose}>
+ *     <ModalHeader />
+ *     <ModalContent>컨텐츠 내용</ModalContent>
+ * </Modal>
+ */
 function Modal({ children, visible, onClose }) {
   const className = '';
   const modalRef = useRef(null);
--- src/components/common/Modal/ModalHeader.jsx
+++ src/components/common/Modal/ModalHeader.jsx
@@ -1,6 +1,12 @@
 import { Icon } from '@/components/common/Icon/Icon.jsx';
 import * as S from './Modal.styles.jsx';
 
+/**
+ * @desc 모달헤더 UI 로 닫기 UI 가 있고, 닫기 기능을 원할 시 props 로 onClose 를 전달해야합니다
+ * @param onClose
+ * @example
+ * <ModalHeader onClose={() => 닫기기능()} />
+ */
 const ModalHeader = ({ onClose }) => {
   return (
     <S.ModalHeader>
