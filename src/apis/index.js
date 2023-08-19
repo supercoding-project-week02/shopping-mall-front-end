3eb81e0628d7dbcaa5ffff07543fa198d1801102
kimseunggyu <dev.seunggyu@gmail.com>
2023-08-19 11:37:30 +0900

    feat : product api 구현

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http:localhost:3000';
+// TODO: .env 로 빼야하나?
+const BASE_URL = 'http://52.79.168.48:8080/api/v1';
 
 export const client = axios.create({
   baseURL: BASE_URL,
