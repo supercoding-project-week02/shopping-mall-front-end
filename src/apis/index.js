4faca9558b0c4151323c7879118c973a32ab37af
kimseunggyu <dev.seunggyu@gmail.com>
2023-08-19 14:18:17 +0900

    feat : 로그인시 localstorage 저장 및 routing 구현

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,21 @@
 import axios from 'axios';
 
+import { localstorageKey } from '@/constant/index.js';
+import { getItem } from '@/utils/localstorage.js';
+
 const BASE_URL = 'http://52.79.168.48:8080/api/v1';
 export const client = axios.create({
   baseURL: BASE_URL,
+});
+
+client.interceptors.request.use(async (config) => {
+  const accessToken = getItem(localstorageKey.auth);
+  // TODO: user 를 전역 상태로 관리하면 해당 부분 삭제
+  // const user = getItem(localstorageKey.user);
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
 });
