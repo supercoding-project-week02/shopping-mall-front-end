06c19ba63d1b26bd3af4da514162822c5d03f793
heelws <162127@naver.com>
2023-08-24 00:19:15 +0900

    feat : 카드 모달 기능 구현

--- src/components/detailModal/CartModal/CartModal.jsx
+++ src/components/detailModal/CartModal/CartModal.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import * as S from './CartModal.styles';
+import { useNavigate } from 'react-router-dom';
 
 export default function CartModal({ isOpen, setIsOpen }) {
+  const navigate = useNavigate();
   const modalIsClose = () => {
     setIsOpen(false);
+  };
+
+  const navigateCartPage = () => {
+    navigate('/cart');
   };
 
   return (
@@ -13,7 +19,7 @@ export default function CartModal({ isOpen, setIsOpen }) {
           <p>선택하신 상품을 장바구니에 담았습니다.</p>
           <S.ModalBtn>
             <S.KeepShoppingBtn onClick={modalIsClose}>계속쇼핑</S.KeepShoppingBtn>
-            <S.GoCartBtn>장바구니</S.GoCartBtn>
+            <S.GoCartBtn onClick={navigateCartPage}>장바구니</S.GoCartBtn>
           </S.ModalBtn>
         </S.ModalContent>
       </S.Modal>
