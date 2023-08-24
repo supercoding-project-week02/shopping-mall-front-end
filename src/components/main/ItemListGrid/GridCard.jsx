71e7ac338a620756f61bd20b8cf331132c9c8c72
Joojue <128790356+Joojue@users.noreply.github.com>
2023-08-24 14:56:18 +0900

    dev 병합

--- src/components/main/ItemListGrid/GridCard.jsx
+++ src/components/main/ItemListGrid/GridCard.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
 
 import imageLoading from '@/assets/genre/imageLoading.png';
 import * as S from './ItemListGrid.Styles.jsx';
