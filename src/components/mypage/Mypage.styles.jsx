6c75ed29df4d1edb456b8da7fd917d05f2359534
kimseunggyu <dev.seunggyu@gmail.com>
2023-08-24 20:41:19 +0900

    feat : 구매자, 판매자 문의 내역 구현

--- src/components/mypage/Mypage.styles.jsx
+++ src/components/mypage/Mypage.styles.jsx
@@ -6,6 +6,7 @@ import TextField from '@/components/common/TextField/TextField.jsx';
 export const ProductInfoWrapper = styled.div`
   display: flex;
   gap: 0.5rem;
+  width: 300px;
 `;
 
 export const Table = styled(_Table)`
@@ -23,3 +24,17 @@ export const InputText = styled(TextField)`
     width: 50px;
   }
 `;
+
+// QuestionCustomerTable
+export const QuestionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+  gap: 0.2rem;
+`;
+
+export const QuestionItemTitle = styled.span`
+  font-weight: bold;
+`;
+
+export const QuestionItemContent = styled.span``;
--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
+import { getQuestionAnswers, getQuestions } from '@/apis/questions.js';
 import PurchaseTable from '@/components/mypage/PurchaseTable.jsx';
+import QuestionCustomerTable from '@/components/mypage/QuestionCustomerTable.jsx';
+import QuestionSellerTable from '@/components/mypage/QuestionSellerTable.jsx';
 import SalesTable from '@/components/mypage/SalesTable.jsx';
 import ShoppingBasketTable from '@/components/mypage/ShoppingBasketTable.jsx';
 import UserProfile from '@/components/UserProfile/UserProfile.jsx';
@@ -9,13 +12,18 @@ import { customerState } from '@/recoil/atoms/userState.js';
 import * as S from './MyPage.styles.jsx';
 
 // const menus = ['판매목록', '구매목록', '장바구니', '문의'];
-const sellerMenus = ['판매목록', '문의'];
-const customerMenus = ['구매목록', '장바구니', '문의'];
+const sellerMenus = ['판매목록', '문의내역'];
+const customerMenus = ['구매목록', '장바구니', '문의내역'];
 const MyPage = () => {
   // 판매목록, 구매목록, 장바구니, 찜목록, 리뷰, 문의
   const isCustomer = useRecoilValue(customerState);
   const menus = isCustomer ? customerMenus : sellerMenus;
   const [activeMenu, setActiveMenu] = useState(menus[0]);
+
+  useEffect(() => {
+    isCustomer && getQuestions().then((data) => console.log('data', data));
+    !isCustomer && getQuestionAnswers().then((data) => console.log('data', data));
+  }, []);
 
   return (
     <S.MyPageWrapper>
@@ -38,6 +46,13 @@ const MyPage = () => {
           {activeMenu === '판매목록' && <SalesTable />}
           {activeMenu === '구매목록' && <PurchaseTable />}
           {activeMenu === '장바구니' && <ShoppingBasketTable />}
+          {activeMenu === '문의내역' ? (
+            isCustomer ? (
+              <QuestionCustomerTable />
+            ) : (
+              <QuestionSellerTable />
+            )
+          ) : null}
         </S.ContentContainer>
       </S.ContentWrapper>
     </S.MyPageWrapper>
